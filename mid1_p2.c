345f16acead211f0b289933065a81d21e144b663
CieranW <cieranwong@gmail.com>
2023-02-23 13:00:57 -0700

    mid1_p2: Added a while loop and Big O notation
    Added a while loop to continue until a negative number has been encountered, in the while loop, updates max and min value if the number inputted is greater or less than the current max and min value. Stated the Big O notation.

--- mid1_p2.c
+++ mid1_p2.c
@@ -2,7 +2,20 @@
 
 int main()
 {
-    int max, min;
+    int max, min, userNum;
 
+    while (userNum > 0) // O(n^2)
+    {
+        scanf("%d", &userNum);
+        if (userNum > max)
+        {
+            max = userNum;
+        }
+        else if (userNum < min)
+        {
+            min = userNum;
+        }
+    }
+    printf("%d", max);
     return 0;
 }
