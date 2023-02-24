fa792e0e7be3dc213eccd75cf0d82950faf1689c
CieranW <cieranwong@gmail.com>
2023-02-24 12:22:34 -0700

    mid1_p3: Added an if else
    If else statement to determine if the initial input is equal to 1 (the criteria to stop the while loop).
    
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- mid1_p3.c
+++ mid1_p3.c
@@ -5,18 +5,27 @@ int main()
     int userNum;
 
     scanf("%d", &userNum);
-
-    while (userNum != 1)
+    if (userNum != 1)
     {
-        if (userNum % 2 == 0)
+        printf("%d\n", userNum);
+        while (userNum != 1)
         {
-            userNum /= 2;
+            if (userNum % 2 == 0)
+            {
+                userNum /= 2;
+            }
+            else if (userNum % 2 == 1)
+            {
+                userNum = (userNum * 3) + 1;
+            }
+            printf("%d\t", userNum);
         }
-        else if (userNum % 2 == 1)
-        {
-            userNum = (userNum * 3) + 1;
-        }
-        printf("%d\t", userNum);
     }
+    else
+    {
+        printf("%d", userNum);
+    }
+
+    printf("\n");
     return 0;
 }
