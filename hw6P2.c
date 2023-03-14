eec4b07b7e807735a22d7b3e8c1ace0bad85f862
CieranW <cieranwong@gmail.com>
2023-03-14 11:44:49 -0700

    HW6P2: Filled out the smallestInt function and main
    In main, declared the file, called the function with pointers, and then priinted out the result.
    
    In smallestInt, took the input, scanned into a temporary variable and in the while loop, used if else statements to adjust based on whether or not the number scanned was the smaller or second smallest value.
    
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- hw6P2.c
+++ hw6P2.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 void smallestInt(FILE *inp, int *pmin1, int *pmin2);
+// pmin1 is the smalles number and pmin2 is the second smallest number, so pmin1 should be less than pmin2.
 
 int main(void)
 {
+    int pmin1, pmin2;
+    FILE *inp;
+    inp = fopen("data.txt", "r");
+
+    smallestInt(inp, &pmin1, &pmin2);
+    printf("Smallest number: %d\n", pmin1);
+    printf("Second smallest number: %d\n", pmin2);
+
+    fclose(inp);
+
+    return 0;
 }
 
 void smallestInt(FILE *inp, int *pmin1, int *pmin2)
 {
+    int num;
+
+    while (!feof(inp))
+    {
+        fscanf(inp, "%d", &num);
+        if (num <= *pmin1)
+        {
+            *pmin2 = *pmin1;
+            *pmin1 = num;
+        }
+        else if (num <= *pmin2 && num != *pmin1)
+        {
+            *pmin2 = num;
+        }
+    }
 }
