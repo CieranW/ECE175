346bf9219ceb924f34bedc2b14bba2cbff90433a
CieranW <cieranwong@gmail.com>
2023-03-21 12:19:32 -0700

    HW7 Part 1: Filled out the main function with four sets of for loops.
    The first loop set each index in the bins array to 0, to signify that they were empty.
    
    The second for loop was a nested for loop with an if else statement. The first for loop represented the players and each one went through the entire bins array looking for multiples of their number (second for loop). The if else statement was to identify if the bins were empty or not and to add/remove items based on the condition met.
    
    Third for loop was to find the position of each bin that was empty and to start counting how many bins are empty.
    
    The last for loop is to print out each position.
    
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- hw7P1.c
+++ hw7P1.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
 #define MAX 1000
 
+// Assume that there are 1,000 bins numbered from 1 to 1000 and 100 players numbered from 1 to 100. Initially, all bins are empty. A player with id x visits all bins that are a multiple of her id. The player will throw a ball in a bin that she visits if the bin is empty, or pick up a ball if the bin has at least one ball. After all 100 players complete their turn, we want to find the number and ids of the empty bins. Write a C program that mimics this experiment and prints a) the number of empty bins, and b) the ids of the empty bins.
+
 int main()
 {
-    int bins[MAX];
+    int bins[MAX], position[1001], i, j, count = 0, k;
+
+    // Setting each index to 0, indicating the bins are empty.
+    for (i = 0; i <= MAX; i++)
+    {
+        bins[i] = 0;
+    }
+
+    for (i = 1; i < 101; i++)
+    {
+        for (j = 1; j < 1001; j++)
+        {
+            if (j % i == 0)
+            {
+                if (bins[j] == 0)
+                {
+                    bins[j]++;
+                }
+                else
+                {
+                    bins[j]--;
+                }
+            }
+        }
+    }
+
+    k = 0;
+    for (i = 1; i < 1001; i++)
+    {
+        if (bins[i] == 0)
+        {
+            count++;
+            position[k++] = i;
+        }
+    }
+
+    printf("Number of empty bins: %d\n", count);
+    for (i = 0; i < k; i++)
+    {
+        printf("%d ", position[i]);
+    }
 
     return 0;
 }
