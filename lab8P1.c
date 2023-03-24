cd8716aa26f74bcedb4f4cfaf94e7e9aa4a05548
CieranW <cieranwong@gmail.com>
2023-03-24 16:00:49 -0700

    Lab 8 Part 1: Init commit
    Description of the problem is to take a array.txt file and use a function to determine if the numbers shown in the square shape are a magic number. Magic squares are defined as having all rows and columns add up to the same number.
    
    Signed-off-by: CieranW <cieranwong@gmail.com>

---
+++ lab8P1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void ReadArray(FILE *inp, int x[][6]);
+
+void ReadArray(FILE *inp, int x[][6])
+{
+}
+
+int main()
+{
+    FILE *inp;
+    inp = fopen("array.txt", "r");
+
+    return 0;
+}
