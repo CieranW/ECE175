b2c856ed93583359d9916d525890b5a9cd2ae77f
CieranW <cieranwong@gmail.com>
2023-04-04 09:59:17 -0700

    Hw 9 Part 1: Init commit
    Initial commit for HW 9 part 1. Description of the problem is to write a program that reverses a string
    
    Signed-off-by: CieranW <cieranwong@gmail.com>

---
+++ hw9p1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char s[]); // function that reverses a string
+
+int main()
+{
+    /* Type your code here. */
+
+    return 0;
+}
+
+void reverse(char s[])
+{
+    /* Function that reverses a string here */
+}
