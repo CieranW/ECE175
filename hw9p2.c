6e7434b2e7bb35bfa73c35820024bd9291b67e5a
CieranW <cieranwong@gmail.com>
2023-04-04 12:50:30 -0700

    Hw 9 Part2: Init Commit
    Program to search for a word in a .txt file and to return the line where the word is located.
    
    Signed-off-by: CieranW <cieranwong@gmail.com>

---
+++ hw9p2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define MAXLINE 1000
+bool lineSearch(char c[], char t[]);
+
+int main(void)
+{
+
+    // your code here.
+    return (0);
+}
+
+bool lineSearch(char c[], char t[])
+{
+
+    // function that returns true if t is found in c.
+}
