16f30de3797acc6d0da83cb80215b67ad5a6de5c
CieranW <cieranwong@gmail.com>
2023-04-04 12:52:14 -0700

    Hw 9 part 1: Filled in the main and reverse function
    Main takes in an input from user, calls reverse. Have not figured out how to terminate the program once the scan and reverse has been completed.
    
    Reverse takes in a word from main and reverses it, printing it out. Prints out a space after the word has been reversed.
    
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- hw9p1.c
+++ hw9p1.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 void reverse(char s[]); // function that reverses a string
 
 int main()
 {
     /* Type your code here. */
+    char userInput[100];
+    bool newLine = false;
+
+    printf("Enter a sentence:\n");
+
+    while (newLine == false && userInput[0] != '\n')
+    {
+        scanf("%s", userInput);
+        reverse(userInput);
+        if (userInput[0] == '\n')
+        {
+            newLine = true;
+        }
+    }
 
     return 0;
 }
@@ -13,4 +28,14 @@ int main()
 void reverse(char s[])
 {
     /* Function that reverses a string here */
+    int len, i, j;
+    char temp;
+
+    len = strlen(s);
+
+    for (i = len - 1; i >= 0; i--)
+    {
+        printf("%c", s[i]);
+    }
+    printf(" ");
 }
